f44fb3e15e1fec10ad784435f47ed5d86dc0e0d0
SowN68 <87044511+SowN68@users.noreply.github.com>
2022-10-17 10:30:23 +0700

    create header

---
+++ src/components/Header/Description.js
@@ -0,0 +1,10 @@
+import "./Description.css"
+const Description = () => {
+     return (
+          <div className="descriptionBox">
+               <h1>A joke a day keeps the doctor away</h1>
+               <p>If you joke wrong way, your teeth have to pay. (Serious)</p>
+          </div>
+     )
+}
+export default Description;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import {Fragment} from "react";
+import Navigation from "./Navigation";
+import Description from "./Description";
+
+const Header = () => {
+     return (
+          <Fragment>
+               <Navigation />
+               <Description/>
+          </Fragment>
+)     
+}
+export default Header;
